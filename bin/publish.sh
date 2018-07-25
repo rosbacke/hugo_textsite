3cd0b3b7597b5fc91271c77b31ebea9b1604a829
Mikael Rosbacke <mikael.rosbacke@gmail.com>
2018-07-26 00:46:56 +0200

    Change theme.

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,5 +2,4 @@
 
 GIT_DIR=$(git rev-parse --show-toplevel)
 
-
-gem install --no-document asciidoctor && hugo -s $GIT_DIR/site -t hugo_theme_aglaus
+gem install --no-document asciidoctor && hugo -s $GIT_DIR/site
